03a5b0ad9baf9730b75920052de13a44e35097a3
LaceyShen <lacey.shen@oocl.com>
2022-07-19 15:43:10 +0800

    project

---
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -0,0 +1,36 @@
+package com.tw;
+
+public class MultiplicationTable {
+    public String create(int start, int end) {
+        if(isValid(start,end)){
+            String multiplicationTable="";
+            multiplicationTable=generateTable(start,end);
+            return multiplicationTable;
+        }
+        return null;
+    }
+
+    public Boolean isValid(int start, int end) {
+        return null;
+    }
+
+    public Boolean isInRange(int number) {
+        return null;
+    }
+
+    public Boolean isStartNotBiggerThanEnd(int start, int end) {
+        return null;
+    }
+
+    public String generateTable(int start, int end) {
+        return null;
+    }
+
+    public String generateLine(int start, int row) {
+        return null;
+    }
+
+    public String generateSingleExpression(int multiplicand, int multiplier) {
+        return null;
+    }
+}
