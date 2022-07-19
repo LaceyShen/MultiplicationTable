0e734abc08ec169d024795d6ab3a9f1345cfd226
LaceyShen <lacey.shen@oocl.com>
2022-07-19 16:25:48 +0800

    add new project

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,36 +1,55 @@
 package com.tw;
 
+import java.util.ArrayList;
+
 public class MultiplicationTable {
+
+    /*public static void main(String[] args) {
+        MultiplicationTable multiplicationTable=new MultiplicationTable();
+        System.out.println(multiplicationTable.create(2,4));
+    }*/
     public String create(int start, int end) {
         if(isValid(start,end)){
             String multiplicationTable="";
             multiplicationTable=generateTable(start,end);
             return multiplicationTable;
+        }else {
+            return null;
         }
-        return null;
     }
 
     public Boolean isValid(int start, int end) {
-        return null;
+        return isInRange(start)&&isInRange(end)&&isStartNotBiggerThanEnd(start,end);
     }
 
     public Boolean isInRange(int number) {
-        return null;
+        return number>=1&&number<=1000;
     }
 
     public Boolean isStartNotBiggerThanEnd(int start, int end) {
-        return null;
+        return start<=end;
     }
 
     public String generateTable(int start, int end) {
-        return null;
+        ArrayList<String> linelist=new ArrayList<>();
+        for (int i = start; i <=end ; i++) {
+            String line=generateLine(start,i);
+            linelist.add(line);
+        }
+        return String.join(System.lineSeparator(),linelist);
     }
 
     public String generateLine(int start, int row) {
-        return null;
+        ArrayList<String> expressionList=new ArrayList<>();
+        for (int i = start; i <=row ; i++) {
+            String expression=generateSingleExpression(i,row);
+            expressionList.add(expression);
+        }
+        return String.join("  ",expressionList);
     }
 
     public String generateSingleExpression(int multiplicand, int multiplier) {
-        return null;
+        String multiplicationExpression=multiplicand+"*"+multiplier+"="+multiplicand*multiplier;
+        return multiplicationExpression;
     }
 }
